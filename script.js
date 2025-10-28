7996504b7f905dca9c6efb681fafcdc14750a949
charlesluo06 <charlesluo06@gmail.com>
2025-10-27 23:42:14 -0700

    added button features, durations, and hardcoded love yourself

--- script.js
+++ script.js
@@ -2,20 +2,35 @@ const playButton = document.getElementById("play");
 const music = document.getElementById("song");
 const pauseButton = document.getElementById("pause");
 const audioSlider = document.getElementById("audioBar");
+const rewindButton = document.getElementById("rewind");
+const nextButton = document.getElementById("next");
+const durationBar = document.getElementById("durationBar");
+const volumeIcon = document.getElementById("volumeIcon");
+let currentTime = document.getElementById("currentTime");
+let totalTime = document.getElementById("totalTime");
 
 music.volume = .25;
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds/60);
+    const secs = Math.floor(seconds % 60);
+    return `${minutes}:${secs< 10 ? "0" + secs : secs}`;
+}
+
+music.addEventListener("loadedmetadata", () => {
+    totalTime.textContent = formatTime(music.duration);
+    durationBar.value = 0;
+});
 
 pauseButton.addEventListener("click", () => {
     if (music.paused) {
         music.play();
-        playButton.textContent = "Play";
         pauseButton.style.opacity = '1';
         playButton.style.opacity = "0";
     }
 
     else {
         music.pause();
-        playButton.textContent = "Pause";
         pauseButton.style.opacity = '0';
         playButton.style.opacity = "1";
     }
@@ -23,4 +38,33 @@ pauseButton.addEventListener("click", () => {
 
 audioSlider.addEventListener("input", ()=> {
     music.volume = audioSlider.value/100;
+});
+
+muteIcon.addEventListener("click", () => {
+    if (music.muted === true) {
+        music.muted = false;
+        muteIcon.style.opacity = '0';
+        volumeIcon.style.opacity = "1";
+    }
+
+    else {
+        music.muted = true;
+        muteIcon.style.opacity = '1';
+        volumeIcon.style.opacity = "0";
+    }
+})
+
+rewindButton.addEventListener("click", () => {  // Add the previous song feature later :))
+    if (music.currentTime >= 2){
+        music.currentTime = 0;
+    }
+});
+
+durationBar.addEventListener("input", () => {
+    music.currentTime = (durationBar.value / 100) * music.duration;
+});
+
+music.addEventListener("timeupdate", () => {
+    durationBar.value = (music.currentTime / music.duration) * 100;
+    currentTime.textContent = formatTime(music.currentTime)
 });
